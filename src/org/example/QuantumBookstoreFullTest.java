819483a1020f6192cb3f5d46957cf2751fb79d06
Malak Mostafa <malakmustafa725@gmail.com>
2025-07-08 21:46:09 +0300

    QuantumBookstoreFullTest.java

--- src/org/example/QuantumBookstoreFullTest.java
+++ src/org/example/QuantumBookstoreFullTest.java
@@ -21,13 +21,15 @@ public class QuantumBookstoreFullTest {
         MailService mailService = new MailService();
         
         BookInventory inventory = new BookInventory();
-        BookInventoryService service = new BookInventoryService(inventory);
+        BookInventoryService bookinventoryservice = new BookInventoryService(inventory);
         
         PaperBook paperBook = new PaperBook(5, "Java Basics", "ISBN001", "John Doe", 2012, new BigDecimal("150.00"), shippingService);
+        PaperBook paperBook01 = new PaperBook(5, "C# Basics", "ISBN004", "John Doe", 2022, new BigDecimal("200.00"), shippingService);
         EBook eBook = new EBook("PDF", "Clean Code", "ISBN002", "Robert Martin", 2020, new BigDecimal("100.00"), mailService);
         ShowcaseBook demoBook = new ShowcaseBook("Sample Book", "ISBN003", "Alice Writer", 2015, new BigDecimal("1.00"));
 
         inventory.addBook(paperBook);
+        inventory.addBook(paperBook01);
         inventory.addBook(eBook);
         inventory.addBook(demoBook);
         
@@ -36,26 +38,33 @@ public class QuantumBookstoreFullTest {
         for (Book b : outdatedBooks) {
             System.out.println("Removed outdated: " + b.getTitle());
         }
-        System.out.println("\n## Buying Paper Book ##");
+        System.out.println("\n## Buying Paper Book  ##");
         try {
-            BigDecimal total = service.buyBook("ISBN001", 2, "user@example.com", "Cairo, Egypt");
+            BigDecimal total = bookinventoryservice.buyBook("ISBN001", 2, "user@example.com", "Cairo, Egypt");
             System.out.println("Total paid for paper book: " + total);
         } catch (OutOfStockException | BookNotFoundException e) {
             System.out.println("Error: " + e.getMessage());
         }
+         System.out.println("\n## Buying Paper Book ##");
+        try {
+            BigDecimal totalPrice = bookinventoryservice.buyBook("ISBN004", 2, "user@example.com", "Cairo, Egypt");
+            System.out.println("Total paid for paper book: " + totalPrice);
+        } catch (OutOfStockException | BookNotFoundException e) {
+            System.out.println("Error with ISBN003: " + e.getMessage());
+        }
         System.out.println("\n ## Buying EBook ##");
-        BigDecimal ebookTotal = service.buyBook("ISBN002", 1, "user@example.com", "N/A");
+        BigDecimal ebookTotal = bookinventoryservice.buyBook("ISBN002", 1, "user@example.com", "N/A");
         System.out.println("Total paid for eBook: " + ebookTotal);
 
         System.out.println("\n ## Trying to Buy Showcase Book ##");
         try {
-            service.buyBook("ISBN003", 1, "user@example.com", "N/A");
+            bookinventoryservice.buyBook("ISBN003", 1, "user@example.com", "N/A");
         } catch (NotForPurchaseException e) {
             System.out.println("Error: " + e.getMessage());
         }
         System.out.println("\n ## Trying to Buy Non-Existing Book ##");
         try {
-            service.buyBook("ISBN999", 1, "user@example.com", "N/A");
+            bookinventoryservice.buyBook("ISBN999", 1, "user@example.com", "N/A");
         } catch (BookNotFoundException e) {
             System.out.println("Error: " + e.getMessage());
         }
